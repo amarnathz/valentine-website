8deb5605a52904c34cfddf776e72f3b11b97114a
Amarnath M <36194961+amarnathz@users.noreply.github.com>
2026-02-08 00:51:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -79,7 +79,6 @@ const CONFIG = {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
         musicUrl: "https://s31.aconvert.com/convert/p3r68-cdx67/3kuby-2z8kg.mp3", // Music streaming URL
-        //musicUrl: "https://jumpshare.com/s/fdcoSTcL0u2U6gSpAWuV", 
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
