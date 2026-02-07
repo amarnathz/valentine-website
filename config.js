0cbd6e27201efaf7f4ed5766e8a5bf5aa4cde66c
Amarnath M <36194961+amarnathz@users.noreply.github.com>
2026-02-08 00:21:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://s33.aconvert.com/convert/p3r68-cdx67/4ccwi-whkrv.mp3", // Music streaming URL
+        musicUrl: "https://s17.aconvert.com/convert/p3r68-cdx67/qslte-vxij9.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
